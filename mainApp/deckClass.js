d7dccad354d3c28818937a09e3bfed8611985386
artem <artem.derkach.gs@gmail.com>
2016-10-19 22:40:55 +0300

    deck class finished

--- mainApp/deckClass.js
+++ mainApp/deckClass.js
@@ -2,36 +2,22 @@
 
 const Card = require('./cardClass.js').Card;
 
+const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 class Deck {
 
   constructor() {
-    this.symbols = {spades: '♠', hearts: '♥', clubs: '♣', diamonds: '♦'};
-    this.suits = ['spades', 'hearts', 'clubs', 'diamonds'];
-    this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     this.cardsInDeck = (() => {
       const createdDeck = [];
-      for (let i = 0; i < this.suits.length; i += 1) {
-        for (let j = 0; j < this.ranks.length; j += 1) {
-          createdDeck.push(new Card(this.ranks[j], this.suits[i], j + 1));
+      for (let i = 0; i < suits.length; i += 1) {
+        for (let j = 0; j < ranks.length; j += 1) {
+          createdDeck.push(new Card(ranks[j], suits[i]));
         }
       }
       return createdDeck;
     })();
     this.cardsOutsideDeck = [];
-  }
-
-  // delete given elements from deck
-  // take [{rank:.., suit:..}, {rank:.., suit:..}, .... ]
-  delete(arr) {
-    arr.forEach((elem) => {
-      for (let j = 0; j < this.deck.length; j += 1) {
-        if (elem.rank === this.deck[j].rank && elem.suit === this.deck[j].suit) {
-          this.deck = this.deck.slice(0, j).concat(this.deck.slice(j + 1, this.deck.length));
-          break;
-        }
-      }
-    });
-    return this.deck;
   }
 
   shuffle() {
@@ -46,35 +32,31 @@ class Deck {
     return this.cardsInDeck;
   }
 
-  // move one card from cardsInDeck to cardsOutsideDeck
-  // and return those card
   dealTopCard() {
-    if (this.deck.length === 0) {
-      throw new Error('no deck left');
+    if (this.cardsInDeck.length === 0) {
+      throw new Error('no cards left');
     }
-    const cardToDeal = this.deck[0];
+    const cardToDeal = this.cardsInDeck.pop();
     this.cardsOutsideDeck.push(cardToDeal);
-    this.cardsInDeck = this.cardsInDeck.slice(1, this.deck.length);
     return cardToDeal;
   }
 
   // deal n cards
-  dealN(n) {
+  dealNCards(n) {
     const result = [];
     for (let i = n; i > 0; i -= 1) {
-      result.push(this.deal());
+      result.push(this.dealTopCard());
     }
     return result;
   }
 
   dealCard(card) {
-    let temp;
-    for (let i = 0; i < this.deck.length; i += 1) {
-      if (card.suit === this.deck[i].suit && card.rank === this.deck[i].rank) {
-        temp = this.deck[0];
-        this.deck[0] = this.deck[i];
-        this.deck[i] = temp;
-        return this.deal();
+    let cardToRemove;
+    for (let i = 0; i < this.cardsInDeck.length; i += 1) {
+      if (card.rank === this.cardsInDeck[i].rank && card.suit.name === this.cardsInDeck[i].suit.name) {
+        cardToRemove = this.cardsInDeck.splice(i, 1)[0];
+        this.cardsOutsideDeck.push(cardToRemove);
+        return cardToRemove;
       }
     }
     return null;
