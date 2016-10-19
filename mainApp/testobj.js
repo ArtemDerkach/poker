fe0a805ccdaf75cf6ab6f0fbb7aa5c040ecab259
artem <artem.derkach.gs@gmail.com>
2016-10-19 23:33:18 +0300

    deleted unnecessery file

--- mainApp/testobj.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let arrayOfPlayers = [1,2,3,4,5];
-
-// arrayOfPlayers = arrayOfPlayers.shift().concat(arrayOfPlayers[0]);
-
-arrayOfPlayers = arrayOfPlayers.slice(1, arrayOfPlayers.length).concat(arrayOfPlayers.slice(0, 1));
-
-console.log(arrayOfPlayers);
